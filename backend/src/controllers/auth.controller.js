1ddf1500ce01d4e5465f4efa021fcf60ec694ec1
Pranav Birla <pranavbirla06@gmail.com>
2026-05-11 01:36:41 +0530

    Solved that logout bug where user can still do a booking after logging out but now solved

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -82,7 +82,12 @@ async function loginUser(req, res) {
 };
 
 function logoutUser(req, res) {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+
     res.status(200).json({
         message: "User LoggedOut successfully"
     });
